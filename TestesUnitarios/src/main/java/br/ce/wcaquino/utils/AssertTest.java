301b2b4c0d9aa4698c76dffa52a3f0cd31b33d2a
antoniobigas <antoniobigas@gmail.com>
2023-01-10 12:32:04 -0300

    Assert

---
+++ TestesUnitarios/src/main/java/br/ce/wcaquino/utils/AssertTest.java
@@ -0,0 +1,18 @@
+package br.ce.wcaquino.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AssertTest {
+	
+	@Test
+	public void test() {
+		Assert.assertTrue(true);
+		Assert.assertFalse(false);
+		
+		
+		Assert.assertEquals(1, 2);
+		
+	}
+
+}
